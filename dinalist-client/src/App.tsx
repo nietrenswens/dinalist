85cc19ed3991e3a4d0127b358f5f991a52256928
Rens <mulderrens@outlook.com>
2024-05-02 11:43:46 +0200

    Added frontend token validation

--- dinalist-client/src/App.tsx
+++ dinalist-client/src/App.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Login from "./Components/Login/Login";
 
-function App() {
-  if (localStorage.getItem("token")) {
+function checkToken(token: string) {
+  const API_HOST: string = process.env.REACT_APP_API_URL || "";
+  fetch(API_HOST + "/api/login/istokenvalid", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.isvalid) {
+        return false;
+      }
+      return true;
+    }).catch((err) => {
+      console.error(err);
+    });
+  return false;
+}
 
-    // Check token validity
-    
+function App() {
+  const token = localStorage.getItem("token");
+  if (token !== null) {
+    if (!checkToken(token))
+    {
+      localStorage.removeItem("token");
+      localStorage.removeItem("id");
+      window.location.href = "/";
+      return;
+    }
     return (
       <div className="h-screen">
         <h1>Logged in</h1>
